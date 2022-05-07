4c170ef8414d3ca89c3dcc4b3596ccf3d100e316
mirakaya <mariajonas.sousa00@gmail.com>
2022-05-07 14:27:19 +0100

    PConsumer - untested

--- src/UC1/PConsumer/PConsumer.java
+++ src/UC1/PConsumer/PConsumer.java
@@ -1,4 +1,4 @@
-package UC1;
+package UC1.PConsumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -13,27 +13,52 @@ import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
-public class PConsumer extends Thread{
-    Properties properties = new Properties();
+public class PConsumer{
+    //Properties properties = new Properties();
     private String topicName = "test";
     private String groupName = "firstGroup";
-    public PConsumer(){
-        properties.put("bootstrap.servers", "localhost:9092");
+
+    private Properties [] propertiesList;
+    private int nr_consumers;
+
+    public PConsumer(Properties [] propertiesList, int nr_consumers){
+        /*properties.put("bootstrap.servers", "localhost:9092");
         properties.put("group.id", groupName);
         properties.put("auto.commit.interval.ms", "1000");
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        */
+        this.propertiesList = propertiesList;
+        this.nr_consumers = nr_consumers;
+
+        createConsumers();
+
     }
 
-    @Override
-    public void run() {
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+    public void createConsumers() {
+        TConsumer [][] listConsumers = new TConsumer[propertiesList.length][nr_consumers];
+
+        //create nr_consumers amount of consumers for each group
+        for (int i = 0; i < propertiesList.length; i++){
+
+            for (int j = 0; j < nr_consumers; j++){
+
+                TConsumer consumer = new TConsumer(propertiesList[i]);
+                consumer.receiveNewTopic(topicName);
+                consumer.start();
+                listConsumers[i][j] = consumer;
+
+            }
+
+        }
+
+        /*KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
         consumer.subscribe(Arrays.asList(topicName));
         System.out.println("consumer begins !");
         while(true){
             ConsumerRecords<String,String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
                 System.out.println("Receive message : " + record.value());
-        }
+        }*/
     }
 }
