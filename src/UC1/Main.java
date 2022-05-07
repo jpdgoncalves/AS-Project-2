b246c328f0451fb542dff7510a22a67e20bf5bd9
José Gonçalves <jpsextafeira@gmail.com>
2022-05-07 15:33:51 +0100

    Adding test file since this is the code that is currently functioning

--- src/UC1/Main.java
+++ src/UC1/Main.java
@@ -1,14 +1,12 @@
 package UC1;
 
-import UC1.PProducer.PProducer;
-
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 //        PProducer producer = new PProducer();
 //        producer.start();
-        PConsumer consumer = new PConsumer();
+        PConsumerTest consumer = new PConsumerTest();
         consumer.start();
 //        PSource source = new PSource();
 
