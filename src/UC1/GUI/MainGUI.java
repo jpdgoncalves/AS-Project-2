5601a42eafaaa0d2719c31b47f8a1a2129792d74
mirakaya <mariajonas.sousa00@gmail.com>
2022-05-09 00:56:43 +0100

    Can send string to be printed by the GUIs. ConsumerGUI.

--- src/UC1/GUI/MainGUI.java
+++ src/UC1/GUI/MainGUI.java
@@ -4,9 +4,24 @@ import java.io.IOException;
 
 public class MainGUI {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-       TUpdateGUIinfo up = new TUpdateGUIinfo();
-       up.start();
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+       TUpdateGUIinfo producergui = new TUpdateGUIinfo("P");
+        producergui.start();
+
+       TUpdateGUIinfo consumergui = new TUpdateGUIinfo("C");
+        consumergui.start();
+
+
+       //test code
+       while (true){
+
+           //bit of code needed to send new info
+           producergui.sendInfo("NO");
+           consumergui.sendInfo("hey");
+           Thread.sleep(1000);
+
+       }
+
     }
 
 }
--- src/UC1/GUI/TUpdateGUIinfo.java
+++ src/UC1/GUI/TUpdateGUIinfo.java
@@ -2,44 +2,97 @@ package UC1.GUI;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TUpdateGUIinfo extends Thread{
 
-    private ProducerGUI gui;
+    private ProducerGUI guiP;
+    private ConsumerGUI guiC;
+    private boolean isAlive = true;
+    private final ReentrantLock rl = new ReentrantLock();
+    private final Condition newData;
+    private boolean hasDataBeenRead;
+    private final String option;
+
 
     private String toWrite;
 
-    public TUpdateGUIinfo() throws IOException, ClassNotFoundException {
-        this.gui = new ProducerGUI();
-        gui.open();
+    //TODO
+    //Do I like this solution of thread monitor like thingy? No.
+    //Does it work? Yes.
 
+    public TUpdateGUIinfo(String guiOption) throws IOException, ClassNotFoundException {
+
+        option = guiOption;
+
+        switch (option){
+            case "P":
+                this.guiP = new ProducerGUI();
+                guiP.open();
+                break;
+            case "C":
+                this.guiC = new ConsumerGUI();
+                guiC.open();
+                break;
+
+        }
+
+
+        this.newData = rl.newCondition();
+        this.hasDataBeenRead = false;
+    }
+
+    public void killThread (){
+        isAlive = false;
+    }
+
+    public void sendInfo(String data){
+        rl.lock();
+
+        toWrite = data;
+        hasDataBeenRead = false;
+        newData.signal();
+
+        rl.unlock();
     }
 
     @Override
     public void run() {
 
+        rl.lock();
+
         final Runnable writeToGUI = new Runnable() {
             public void run() {
-                gui.setContent(toWrite);
+                switch (option){
+                    case "P":
+                        guiP.setContent(toWrite);
+                        break;
+                    case "C":
+                        guiC.setContent(toWrite);
+                        break;
+                }
+
             }
         };
 
-        while (true){
+        while (isAlive){ //boolean that can be set by whoever creates the thread
             try {
-                //TODO - missing receiving info from producer
-                toWrite = "hi";
+
+                while (hasDataBeenRead == true || toWrite == null) {  // no new data to read
+                    newData.await();
+                }
+
                 SwingUtilities.invokeAndWait(writeToGUI);
+                hasDataBeenRead = true;
             }
             catch (Exception e) {
                 e.printStackTrace();
             }
-            try {
-                sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
         }
 
+        rl.unlock();
 
     }
 
