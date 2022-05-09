d3865486142be3d20a3a5b37f12a06864f309eb4
mirakaya <mariajonas.sousa00@gmail.com>
2022-05-09 12:31:07 +0100

    Consumer changes.

--- src/UC1/PConsumer/PConsumer.java
+++ src/UC1/PConsumer/PConsumer.java
@@ -1,10 +1,13 @@
 package UC1.PConsumer;
 
 import UC1.PProducer.TProducer;
+import org.apache.kafka.common.TopicPartition;
 
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class PConsumer{
@@ -12,6 +15,7 @@ public class PConsumer{
     public static void main(String[] args) {
         String topicName = "sensor";
         String groupName = "firstGroup";
+        int partition = 0;
 
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
@@ -19,7 +23,10 @@ public class PConsumer{
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-        TConsumer consumer = new TConsumer(props, topicName);
+        TopicPartition topicPartition = new TopicPartition(topicName, partition);
+        List<TopicPartition> asList = Arrays.asList(topicPartition);
+
+        TConsumer consumer = new TConsumer(props/*, topicName*/, asList);
         consumer.start();
     }
 
--- src/UC1/PConsumer/TConsumer.java
+++ src/UC1/PConsumer/TConsumer.java
@@ -3,25 +3,33 @@ package UC1.PConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class TConsumer extends Thread{
     private Properties properties;
     private KafkaConsumer<String, String> consumer;
 
+    private List <TopicPartition> topicPartitions;
+
     private String topicName;
 
-    public TConsumer(Properties properties, String newTopic){
+    public TConsumer(Properties properties/*, String newTopic*/, List <TopicPartition> topicPartitions){
         this.properties = properties;
         this.consumer = new KafkaConsumer<>(this.properties);
-        this.topicName = newTopic;
+        //this.topicName = newTopic;
+        this.topicPartitions = topicPartitions;
+
     }
 
     @Override
     public void run() {
-        consumer.subscribe(Arrays.asList(topicName));
+        //consumer.subscribe(Arrays.asList(topicName));
+        consumer.assign(topicPartitions);
+
         System.out.println("consumer begins !");
         while(true){
             ConsumerRecords<String,String> records = consumer.poll(100);
