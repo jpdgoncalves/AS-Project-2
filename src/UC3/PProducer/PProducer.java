e4b41b0f2393aa4f9be0e6446ea49c1e88cc1886
mathebrard <mathebrard47@gmail.com>
2022-05-10 15:45:46 +0100

    adding UC3

---
+++ src/UC3/PProducer/PProducer.java
@@ -0,0 +1,45 @@
+package UC3.PProducer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Properties;
+
+public class PProducer{
+
+
+    public static final int PSOURCE_PORT = 13000;
+    public static final String IP_ADDRESS = "127.0.0.1";
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+
+
+        TProducer producers[] = new TProducer[3];
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        //records can be lost :
+        props.put("acks", "0");
+        //to make performance better since records can be lost
+        props.put("retries", "0");
+
+        //starting 3 producers
+        for (int i=0; i<3; i++){
+            //Socket attributes
+            Socket clientSocket = new Socket(IP_ADDRESS, PSOURCE_PORT);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+            producers[i] = (new TProducer(props, in));
+            producers[i].start();
+        }
+
+    }
+
+
+}
---
+++ src/UC3/PSource/TSender.java
@@ -0,0 +1,58 @@
+package UC3.PSource;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class TSender extends Thread {
+
+    private final ObjectOutputStream out;
+    private final SensorReader sensorReader;
+
+    public TSender(Socket sendSocket, SensorReader sensorReader) throws IOException {
+        this.sensorReader = sensorReader;
+        this.out = new ObjectOutputStream(sendSocket.getOutputStream());
+
+        setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+        this.log("Started!");
+
+        try {
+            String sensorId = sensorReader.getSensorId();
+            SensorData data;
+
+            if (sensorId == null) {
+                this.log("No more sensorIds left. Stopping!");
+                return;
+            }
+
+
+            while ((data = sensorReader.getData(sensorId)) != null) {
+                this.log("Sending " + data);
+                this.out.writeObject(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            cleanup();
+        }
+
+        this.log("Finished!");
+    }
+
+    private void cleanup() {
+        try {
+            this.out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.log("Finished cleanup!");
+    }
+
+    private void log(String msg) {
+        System.out.println("Sender thread " + this.getId() + ": " + msg);
+    }
+}
