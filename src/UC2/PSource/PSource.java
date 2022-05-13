9eabe83988da22c560e28f02f5ced78cbb17d4cf
José Gonçalves <jpsextafeira@gmail.com>
2022-05-13 11:33:04 +0100

    Making sure PSource finishes after either crashes occur or everything is read

--- src/UC2/PSource/PSource.java
+++ src/UC2/PSource/PSource.java
@@ -5,22 +5,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class PSource {
-    public static final int PSOURCE_PORT = 13000;
+    private static final int PSOURCE_PORT = 13000;
+    private static final int producerCount = 6;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         DataBufferMap buffers = new DataBufferMap();
         ServerSocket serverSocket = new ServerSocket(PSOURCE_PORT);
+        TReader tReader = new TReader("sensor.txt", buffers);
+        TSender[] tSenders = new TSender[producerCount];
 
         System.out.println("PSource process is running!");
-        TReader tReader = new TReader("sensor.txt", buffers);
         tReader.start();
 
-        while (true) {
+        for (int i = 0; i < producerCount; i++) {
             Socket socket = serverSocket.accept();
             System.out.println("Accepted a new socket connection! Starting thread...");
-            (new TSender(socket, buffers)).start();
+            TSender tSender = new TSender(socket, buffers);
+            tSender.start();
+            tSenders[i] = tSender;
         }
 
+        for (int i = 0; i < producerCount; i++) {
+            tSenders[i].join();
+        }
+
+        tReader.join();
+
+        System.out.println("Finished");
     }
 }
